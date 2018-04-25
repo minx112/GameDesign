8bf5e941642a423c9cda9ed844cf8fbbefd3d15c
Mitchell Dennen <mitchelldennen@aol.com>
2018-04-24 18:42:31 -0700

    health ui

---
+++ finalProject/Infected/Assets/Scripts/HUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+	public Sprite[] HeartsSprites;
+	public Image HeartUI;
+	private PlayerController player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();//Components
+
+	}
+	void Update() {
+		//HeartUI.sprite = HeartsSprites[player.curHealth];
+
+	}
+}
