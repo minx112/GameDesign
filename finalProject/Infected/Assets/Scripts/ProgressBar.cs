ae47fc81fd0499f6af091ba8671f1c4e0ec5206a
Mitchell Dennen <mitchelldennen@aol.com>
2018-04-30 16:44:28 -0700

    this

---
+++ finalProject/Infected/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressBar : MonoBehaviour {
+
+	public Slider progress;
+	Stopwatch watch;
+	private int time;
+	// Use this for initialization
+	//becoming insane 7:20 - 440 secs
+	//1.000   440
+	void Start () {
+		progress.value = 0;
+		watch = new Stopwatch ();
+		time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		time = watch.ElapsedMilliseconds;
+		if(time%1000 == 0) {
+			progress.value += .07;
+	
+	}
+}
