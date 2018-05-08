30153b4cfeedd2e4746db98f66dbc72447c7a94b
Mitchell Dennen <mitchelldennen@aol.com>
2018-05-08 16:31:37 -0700

    progress bar color

--- finalProject/Infected/Assets/Scripts/ProgressBar.cs
+++ finalProject/Infected/Assets/Scripts/ProgressBar.cs
@@ -6,30 +6,28 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ProgressBar : MonoBehaviour {
-
+	public Color yellow = Color.yellow;
+	public Color magenta = Color.magenta;
+	public Color blue = Color.blue;
+	public Color cyan = Color.cyan;
+	public Color red = Color.red;
+	public Color green = Color.green;
+	public Image Fill;
 	public Slider progress;
-	Stopwatch watch;
-	private long time;
 	private int counter = 0;
-	// Use this for initialization
 	//becoming insane 7:20 - 440 secs
-	//1.000   440
 	void Start () {
 		progress.value = 0;
-		watch = new Stopwatch ();
-		time = 0;
 		InvokeRepeating("Prog", 0.0f, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		/*time = watch.ElapsedMilliseconds;
-		if (watch.ElapsedMilliseconds % 1000 == 0) {
-			//Debug.Log (time);
-			progress.value += .07f;
-		}*/
-	
+		if (counter >= 5) {
+			Fill.color = Color.Lerp (yellow, blue, Mathf.PingPong (Time.time, 1));
+		} else if (counter >= 10) { 
+
+		}
 	}
 	void Prog() {
 		progress.value += .0022727f;
